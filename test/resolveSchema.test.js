79ea7d52d9b060dbbb9598b43a67847874704907
Damian Marek <damian.marek@ynd-consult.com>
2019-12-20 22:40:15 +0100

    Support nested function resolving

--- test/resolveSchema.test.js
+++ test/resolveSchema.test.js
@@ -19,4 +19,35 @@ describe('resolveSchema', () => {
       fullName: 'Damian Marek',
     })
   })
+
+  it('should return data with resolved one level nested function', () => {
+    const data = {
+      firstName: 'Damian',
+      lastName: 'Marek',
+      address: {
+        city: 'Warsaw',
+        country: 'Poland',
+      },
+    }
+
+    const schema = {
+      fullName: ({ firstName, lastName }) => `${firstName} ${lastName}`,
+      address: {
+        fullAddress: ({ city, country }) => `${city}, ${country}`,
+      },
+    }
+
+    const resolvedData = resolveSchema(schema, data)
+
+    expect(resolvedData).toEqual({
+      firstName: 'Damian',
+      lastName: 'Marek',
+      fullName: 'Damian Marek',
+      address: {
+        city: 'Warsaw',
+        country: 'Poland',
+        fullAddress: 'Warsaw, Poland',
+      },
+    })
+  })
 })
